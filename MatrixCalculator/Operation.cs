a8fe57d7e278232eb99234cbd82052bfcd4aeab6
makeitokay <andrey.mu11@yandex.ru>
2022-04-09 15:42:24 +0300

    separate file for enum "Operation"

---
+++ MatrixCalculator/Operation.cs
@@ -0,0 +1,26 @@
+﻿namespace MatrixCalculator
+{
+    // Все возможные операции для выполнения
+    public enum Operation
+    {
+        MatrixTrace = 1,
+        MatrixTranspose,
+        MatrixAddition,
+        MatrixDifference,
+        MatrixMultiplication,
+        MatrixMultiplyingByNumber,
+        MatrixDeterminant,
+        SystemOfAlgebraicEquations,
+
+        MainMatrixInput,
+        AdditionalMatrixInput,
+        SwapMainAndAdditionalMatrix,
+        PrintMainMatrix,
+        PrintAdditionalMatrix,
+        ChangeRandomGeneration,
+
+        PrintOperations,
+
+        ProgramExit
+    }
+}
--- MatrixCalculator/Program.cs
+++ MatrixCalculator/Program.cs
@@ -12,30 +12,6 @@ namespace MatrixCalculator
     /// </summary>
     class Program
     {
-        // Все возможные операции для выполнения
-        private enum Operation
-        {
-            MatrixTrace = 1,
-            MatrixTranspose,
-            MatrixAddition,
-            MatrixDifference,
-            MatrixMultiplication,
-            MatrixMultiplyingByNumber,
-            MatrixDeterminant,
-            SystemOfAlgebraicEquations,
-
-            MainMatrixInput,
-            AdditionalMatrixInput,
-            SwapMainAndAdditionalMatrix,
-            PrintMainMatrix,
-            PrintAdditionalMatrix,
-            ChangeRandomGeneration,
-
-            PrintOperations,
-
-            ProgramExit
-        }
-
         // Разбиение операций на категории
         private static readonly List<Operation> CalculationOperations = new()
         {
